d085633af21d95599ec10f488c35834f275a44e4
saganatt <8majak8@gmail.com>
2021-01-01 23:39:36 +0100

    Back to stable tensorflow as it supports CUDA 11 now

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
   install_requires=[ "numpy==1.18.5", "pandas", "matplotlib", "scipy==1.4.1", "h5py<3.0.0",
-                     "keras==2.3.1", "PyYaml", "RootInteractive", "tf-nightly-gpu==2.4.0.dev20200908",
+                     "keras==2.3.1", "PyYaml", "RootInteractive", "tensorflow>=2.4",
                      "pydot", "pylint", "notebook", "torch"],
 
   python_requires='>=3.6, <3.7',
