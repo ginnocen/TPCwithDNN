8ecaa63f40902e9278959bf5b76a9258a3a67739
saganatt <8majak8@gmail.com>
2021-02-20 16:47:58 +0100

    Bigger font and more ticks

--- tpcwithdnn/draw_tb_scalars.py
+++ tpcwithdnn/draw_tb_scalars.py
@@ -3,6 +3,7 @@ import datetime
 import re
 
 import numpy as np
+from matplotlib import rc
 from matplotlib import pyplot as plt
 from matplotlib import cm
 
@@ -13,7 +14,8 @@ def plot_scalar():
                        "nEv([0-9]+)_([a-z]+)-tag-epoch_root_mean_squared_error.csv$")
     csv_files = list(sorted(filter(regex.match, os.listdir(csv_path))))
 
-    fig = plt.figure()
+    rc("font",**{"family":"sans-serif","sans-serif":["Helvetica"], "size": 18})
+    fig = plt.figure(figsize=(12, 7))
     ax = plt.axes()
     for ind, csv_file in enumerate(csv_files):
         file_search = re.search(regex, csv_file)
@@ -26,7 +28,8 @@ def plot_scalar():
     ax.set_xlabel("Epochs")
     #ax.set_ylim([0., 0.012])
     #ax.set_xlim([-1., 21])
-    ax.set_xticks(list(range(0, 22, 2)))
+    ax.set_yticks(np.arange(0, 0.25, 0.02))
+    ax.set_xticks(np.arange(0, 22, 2))
     ax.set_ylabel("$\it{RMSE}$")
     leg_title = r"Train setup: $\it{N}_{ev}^{training}, \it{n}_{\it{\varphi}}$" +\
                 r"$ \times \it{n}_{\it{r}} \times \it{n}_{\it{z}}$"
@@ -37,7 +40,7 @@ def plot_scalar():
     date = datetime.date.today().strftime("%Y%m%d")
     out_filename = "%s_tb_scalar" % date
     for ff in file_formats:
-        fig.savefig("%s.%s" % (out_filename, ff), bbox_inches='tight')
+        fig.savefig("%s.%s" % (out_filename, ff), bbox_inches="tight")
     plt.close(fig)
 
 if __name__ == "__main__":
